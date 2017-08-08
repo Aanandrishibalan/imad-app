d0c09ae1411e3d41a7c4e640d369e67fd0befb4b
Aanandrishibalan <30724270+Aanandrishibalan@users.noreply.github.com>
2017-08-08 10:18:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,59 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var newpage1={
+    title: 'PAGE 1',
+    heading: 'page 1',
+    date: '1-5-2017',
+    content:`  <p>
+    This is my First page. look thirsty I know 
+    </p>
+    <p>This is my First page. look thirsty I know</p>   ` 
+    
+};
+function createtemplate(data){
+var title = data.title;
+var date = data.date;
+var heading = data.heading;
+var content = data.content;
+
+var htmltemplate=`
+<!DOCTYPE html>
+<html>
+    
+<head>
+   
+<title>
+    ${title}
+    </title>
+     <meta name="viewport" content="width=device-width, initial-scale=1"/>
+     <link href="/ui/style.css" rel="stylesheet"/>
+</head>
+<body>
+<div class="container">
+    <div>
+    ${date}
+    </div>
+    <a href="/"> HOME</a>
+    <h1> ${heading}</h1>
+        ${content}
+            
+    
+    </div>
+    
+
+
+</body>
+</html>`;
+return htmltemplate;
+}
 app.get('/newpage1', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'newpage1.html'));
+  res.send(createtemplate(newpage1));
+});
+
+
+app.get('/newpage2', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'newpage2.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
