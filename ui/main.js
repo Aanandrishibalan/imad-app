594ae218c86ea90dddc0b5f49d664a71bc812293
Aanandrishibalan <30724270+Aanandrishibalan@users.noreply.github.com>
2017-08-09 10:10:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'This is another waytext change instead of origin';
+var h1 = document.getElementById('heading');
+h1.onclick = function(){
+ h1.style.marginleft = '200px';    
+};
