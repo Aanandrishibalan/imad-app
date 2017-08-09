1907fe051faa6a4bf574c383367f9d2a8a77a2d6
Aanandrishibalan <30724270+Aanandrishibalan@users.noreply.github.com>
2017-08-09 10:53:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,5 @@
 console.log('Loaded!');
-var element = document.getElementById('main-text');
 
-element.innerHTML = 'This is another waytext change instead of origin';
 var img = document.getElementById('mmm');
 var marginLeft = 0;
 function moveRight () {
