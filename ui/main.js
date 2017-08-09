d77cbd65fcb0eabdb6fcf9dcd124138b2f88d8df
Aanandrishibalan <30724270+Aanandrishibalan@users.noreply.github.com>
2017-08-09 10:28:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,12 @@ var element = document.getElementById('main-text');
 
 element.innerHTML = 'This is another waytext change instead of origin';
 var img = document.getElementById('mmm');
+var marginLeft = 0;
+function moveRight () {
+marginLeft = marginLeft + 10;
+img.style.marginLeft = marginLeft + 'px';
+}
+
 img.onclick = function () {
- img.style.marginLeft = '200px';    
+ var interval = setInterval(moveRight, 100);   
 };
