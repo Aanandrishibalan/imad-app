70f27c6d5512f68d0ce6935cfc62a976cb9eb9b9
Aanandrishibalan <30724270+Aanandrishibalan@users.noreply.github.com>
2017-08-09 10:15:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var element = document.getElementById('main-text');
 
 element.innerHTML = 'This is another waytext change instead of origin';
 var img = document.getElementById('mmm');
-img.onclick = function(){
+img.onclick = function () {
  img.style.marginleft = '200px';    
 };
