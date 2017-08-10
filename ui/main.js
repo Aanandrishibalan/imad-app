9762529e3acc87307456bb375890aa5b3613553f
Aanandrishibalan <30724270+Aanandrishibalan@users.noreply.github.com>
2017-08-10 13:08:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,18 +18,30 @@ button.onclick = function(){
  request.send(null);
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('namebtn');
 submit.onclick = function() {
-    
-    var names = ['name1','name2','name3','name4'];
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE){
+            if (request.status === 200){
+                var names = request.responseText;
+               names = JSON.parse(names);
     var list ='';
     for(var i=0; i< names.length; i++){
         list += '<li>' + names[i] + '<li>';
     }
     var url = document.getElementById('namelist');
     url.innerHTML = list;
+                 
+            }
+        }
+    };
+    var nameInput = document.getElementById('name');
+var name = nameInput.value;
+ request.open('GET', 'http://anandrajd48.imad.hasura-app.io/submit-name?name=' + name , true);
+ request.send(null);
+    
 };
 
 
